8a68b8189aa51c6c921844645779267edd11c98d
Lucas Rafael Ricci <lucasrafael.ricci@gmail.com>
2020-05-05 02:28:05 -0300

    Refatorando o código

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
   'use strict';
 
   var app = (function() {
-    var tbody = $('[data-js="table-car"]').get();
-
     return {
       init: function init() {
         this.companyInfo();
@@ -42,51 +40,42 @@
       createNewCar: function createNewCar() {
         var $fragment = doc.createDocumentFragment();
         var $tr = doc.createElement('tr');
-        var $tdImagem = doc.createElement('td');
-        var $imagem = doc.createElement('img');
-        var $tdMarcaModelo = doc.createElement('td');
-        var $tdAno = doc.createElement('td');
-        var $tdCor = doc.createElement('td');
-        var $tdPreco = doc.createElement('td');
-        var $tdImgRemoverCarro = doc.createElement('td');
-        var $imgRemoverCarro = doc.createElement('img');
+        var $tdImageCar = doc.createElement('td');
+        var $imageCar = doc.createElement('img');
+        var $brand = doc.createElement('td');
+        var $tdYear = doc.createElement('td');
+        var $tdColor = doc.createElement('td');
+        var $tdPrice = doc.createElement('td');
+        var $tdImgRemoveCar = doc.createElement('td');
+        var $imgRemoveCar = doc.createElement('img');
 
-        $imagem.setAttribute('src', $('[data-js="imagem"]').get().value);
-        $tdImagem.appendChild($imagem);
+        $imageCar.setAttribute('src', $('[data-js="imagem"]').get().value);
+        $tdImageCar.appendChild($imageCar);
 
-        $imgRemoverCarro.setAttribute('car-id', this.totalCars());
-        $imgRemoverCarro.setAttribute('src', 'imagens/botao-remover.png');
-        $imgRemoverCarro.setAttribute('width', '50px');
-        $imgRemoverCarro.addEventListener('click', this.removeCar, false);
-        $tdImgRemoverCarro.appendChild($imgRemoverCarro);
+        $imgRemoveCar.setAttribute('src', 'imagens/botao-remover.png');
+        $imgRemoveCar.setAttribute('width', '50px');
+        $imgRemoveCar.addEventListener('click', this.removeCar, false);
+        $tdImgRemoveCar.appendChild($imgRemoveCar);
         
-        $tdMarcaModelo.textContent = $('[data-js="marca-modelo"]').get().value;
-        $tdAno.textContent = $('[data-js="ano"]').get().value;
-        $tdCor.textContent = $('[data-js="cor"]').get().value;
-        $tdPreco.textContent = $('[data-js="preco"]').get().value;
+        $brand.textContent = $('[data-js="marca-modelo"]').get().value;
+        $tdYear.textContent = $('[data-js="ano"]').get().value;
+        $tdColor.textContent = $('[data-js="cor"]').get().value;
+        $tdPrice.textContent = $('[data-js="preco"]').get().value;
 
-        $tr.appendChild($tdImagem);
-        $tr.appendChild($tdMarcaModelo);
-        $tr.appendChild($tdAno);
-        $tr.appendChild($tdCor);
-        $tr.appendChild($tdPreco);
-        $tr.appendChild($tdImgRemoverCarro);
+        $tr.appendChild($imageCar);
+        $tr.appendChild($brand);
+        $tr.appendChild($tdYear);
+        $tr.appendChild($tdColor);
+        $tr.appendChild($tdPrice);
+        $tr.appendChild($tdImgRemoveCar);
 
         this.clearFormData();
+
         return $fragment.appendChild($tr);
       },
       
       removeCar: function removeCar(e) {
-        var target = e.target;
-        var removeTr = target.parentNode.parentNode;
-        
-        if(target.hasAttribute('car-id')) {
-          tbody.removeChild(removeTr);
-        }
-      },
-
-      totalCars: function totalCars() {
-        return tbody.children.length;
+        e.target.parentNode.parentNode.remove();
       },
 
       clearFormData: function clearFormData() {
